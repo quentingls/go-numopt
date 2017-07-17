bdd25d1eac3b08c5feb6eaa63f7ff0832b012192
Quentin Glaisner <quentin.glaisner@gmail.com>
2017-07-17 22:42:03 +0100

    implementing Newton Raphson optimisation

---
+++ newton/newton.go
@@ -0,0 +1,49 @@
+package newton
+
+import (
+	"errors"
+	_ "fmt"
+	"gonum.org/v1/gonum/mat"
+)
+
+// TwiceDifferenciableFunction should implement grandient and henssian in order
+// to create a second degree Taylor expasion.
+type TwiceDifferenciableFunction interface {
+	ValueAt(x *mat.Vector) float64
+	GradientAt(x *mat.Vector) *mat.Vector
+	HenssianAt(x *mat.Vector) *mat.Dense
+}
+
+// OptimisationParameters contains the parameters defining the behaviour of the
+// optimisation iteration. X0 is the starting point, Alpha is the learning rate,
+// Epsilon the targeted accuracy and N the maximum iteration.
+type OptimisationParameters struct {
+	X0      *mat.Vector
+	Alpha   float64
+	Epsilon float64
+	N       int
+}
+
+// NewtonRaphsonOptimise implements the Newton Raphson Optimisation algorithm
+// on the twice-differentiale function f.
+func NewtonRaphsonOptimise(f TwiceDifferenciableFunction, param OptimisationParameters) (*mat.Vector, error) {
+	x := param.X0
+	for i := 0; i < param.N; i++ {
+		henssian, gradient := f.HenssianAt(x), f.GradientAt(x)
+		r, _ := henssian.Dims()
+		invertHenssian := mat.NewDense(r, r, nil)
+		invertHenssian.Inverse(henssian)
+		prod := mat.NewDense(r, 1, nil)
+		prod.Mul(invertHenssian, gradient)
+		d := prod.ColView(0)
+		d.ScaleVec(-param.Alpha, d)
+		x.SubVec(x, d)
+		if f.ValueAt(x) < param.Epsilon {
+			return x, nil
+		}
+	}
+	return x, errors.New("maximum number of iteraton has been reached")
+}
+
+// TODO: Implement backtracking for step size optimisation
+func backtracking() {}
---
+++ newton/newton_test.go
@@ -0,0 +1,49 @@
+package newton
+
+import (
+	_ "fmt"
+	"gonum.org/v1/gonum/mat"
+	"testing"
+)
+
+type function struct {
+}
+
+// f(x, y) = x (2x - y) + 2y
+// grad(f)(x,y) = [4x - y, -x + 2]
+// henssian(f) (x,y) = |4,-1|
+// 					   |-1,0|
+
+func (f function) ValueAt(x *mat.Vector) float64 {
+	x1, x2 := x.At(0, 0), x.At(1, 0)
+	return x1*(2*x1-x2) + 2*x2
+}
+
+func (f function) GradientAt(x *mat.Vector) *mat.Vector {
+	x1, x2 := x.At(0, 0), x.At(1, 0)
+	value := []float64{4*x1 - x2, -x1 + 2}
+	return mat.NewVector(2, value)
+}
+
+func (f function) HenssianAt(x *mat.Vector) *mat.Dense {
+	value := []float64{
+		4, -1,
+		-1, 0,
+	}
+	return mat.NewDense(2, 2, value)
+}
+
+func TestNewtonRaphson(t *testing.T) {
+	x0Value := []float64{1, 1}
+	param := OptimisationParameters{
+		X0:      mat.NewVector(2, x0Value),
+		Alpha:   0.01,
+		Epsilon: 0.01,
+		N:       100,
+	}
+	res, err := NewtonRaphsonOptimise(function{}, param)
+	if err != nil {
+		t.Errorf("should have nil error, having: %s", err.Error())
+	}
+	t.Errorf("having res: %v", res)
+}
